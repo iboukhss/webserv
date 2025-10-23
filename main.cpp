79596efeb453615c58ae5b6604a3c2ab31bb8624
Heck Dennis <heckdennis257@gmail.com>
2025-10-23 19:55:21 +0200

    updating main by calling new methods

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include "Server.hpp"
 
 #include "unistd.h"
-#include <cstring>
 #include <iostream>
 #include <netinet/in.h>
 #include <vector>
@@ -13,7 +12,7 @@
 
 int main(void)
 {
-    Server server(1000);
+    Server server(8080, 1000); // later params retrieved from config file
     int server_fd = server.getFd();
     Epoll epoll(server.getFd());
     int n_events;
@@ -23,26 +22,17 @@ int main(void)
         // two solutions possible : wait returns n events -> either loop 0 to n in the epoll_event
         // array held by the epoll object or modify the wait function to return an array
         n_events = epoll.wait();
-        const epoll_event* events = epoll.getEvents();
         for (int i = 0; i < n_events; ++i) {
-            if (events[i].data.fd == server_fd) {
+            int event_fd = epoll.getEventFd(i);
+            if (event_fd == server_fd) {
                 // we need to accept a new connection(s) -> there might be a queu
-                while (1) {
-
-                    struct sockaddr_in client_addr;
-                    socklen_t client_len = sizeof(client_addr);
-                    int client_fd = accept(
-                        server_fd, (struct sockaddr*)&client_addr,
-                        &client_len); // extracts the  first  connection  request  on  the  queue
-                                      // of pending connections for the listening socket
+                while (true) {
+                    int client_fd = server.acceptConn();
                     if (client_fd == -1) {
-                        // error occured or no further connection request to be handled -> fine
-                        // tuning later
                         break;
                     }
-                    std::cout << "server event fd = " << events[i].data.fd
-                              << "client_fd = " << client_fd << std::endl;
-                    server.set_nonblocking(client_fd);
+                    std::cout << "server event fd = " << event_fd << "client_fd = " << client_fd
+                              << std::endl;
                     epoll.add(client_fd, EPOLLIN);
                 }
             }
@@ -53,9 +43,10 @@ int main(void)
                                        "Content-Type: text/plain\r\n"
                                        "\r\n"
                                        "Hello, client!\n";
-                send(events[i].data.fd, response, strlen(response), 0);
-                epoll.remove(events[i].data.fd);
-                close(events[i].data.fd);
+                server.sendResponse(event_fd, response);
+                epoll.remove(event_fd);
+                close(event_fd); // not needed as by removing the event_fd from the interest list,
+                                 // the fd is closed ?
             }
         }
     }
