891796b03ab6f22ac9a5ddaf5539da62abef5666
Heck Dennis <heckdennis257@gmail.com>
2025-10-28 08:02:16 +0100

    feat(router): add main logic

--- main.cpp
+++ main.cpp
@@ -1,5 +1,9 @@
+
+
+// #include "ConfigParser.hpp"
 #include "Server.hpp"
 #include "SyscallError.hpp"
+#include "structs_dev.hpp" //to be removed
 
 #include <iostream>
 
@@ -11,10 +15,61 @@ extern "C" void handle_sigint(int sig)
     g_sigint_received = 1;
 }
 
+void test_config1(ServerConfig& config)
+{
+    // ------------------------
+    // Primitive fields
+    // ------------------------
+    config.listen_port = 8080;
+    config.default_server = true;
+    config.root = "/home/dennis/Documents/42Luxembourg/Core/webserv";
+    config.domain = AF_INET;
+    config.max_conn = 1000;
+    config.protocol = "http";
+
+    // ------------------------
+    // Server names
+    // ------------------------
+    config.server_name.push_back("test_server");
+
+    // ------------------------
+    // Allowed methods
+    // ------------------------
+    config.methods.push_back("GET");
+    config.methods.push_back("POST");
+    config.methods.push_back("DELETE");
+
+    // ------------------------
+    // Default location
+    // ------------------------
+    config.default_location.path = "/home/dennis/Documents/42Luxembourg/Core/webserv";
+    config.default_location.index = "index.html";
+
+    // ------------------------
+    // Other locations
+    // ------------------------
+    Location loc1;
+    loc1.path = "/pages";
+    loc1.index = "index.html";
+    config.locations.push_back(loc1);
+
+    Location loc2;
+    loc2.path = "/images";
+    loc2.index = "index.html";
+    config.locations.push_back(loc2);
+
+    Location loc3;
+    loc3.path = "/files";
+    loc3.index = "index.html";
+    config.locations.push_back(loc3);
+}
+
 int main(void)
 {
-    // later params retrieved from config file
-    Server server(INADDR_ANY, 8080, 1000);
+    ServerConfig config;
+    test_config1(config);
+
+    Server server(INADDR_ANY, 8080, 1000, &config);
 
     struct sigaction sa;
     sa.sa_handler = handle_sigint;
---
+++ structs_dev.hpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+
+#ifndef STRUCTS_DEV_HPP
+#define STRUCTS_DEV_HPP
+// for dev purpose only
+struct Location {
+    std::string path;
+    std::string index;
+};
+
+struct ServerConfig {
+    int listen_port;                      // port to listen
+    std::vector<std::string> server_name; // host names
+    bool default_server; // default server applies to a port and if none is flagged default_server,
+                         // then the first one is the default one
+    std::vector<std::string> methods; // allowed methods
+    std::string root;
+    Location default_location;
+    std::vector<Location> locations;
+    int domain;
+    int max_conn;
+    std::string protocol;
+};
+
+struct HttpRequest {
+    std::string method;
+    std::string path;
+};
+
+#endif
