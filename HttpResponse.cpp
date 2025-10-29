c54bd06c9b648f3f45d15a7445a21de594e6da0f
Ismail <151830328+iboukhss@users.noreply.github.com>
2025-10-29 08:33:29 +0100

    feat: add HttpResponse class

---
+++ HttpResponse.cpp
@@ -0,0 +1,22 @@
+#include "HttpResponse.hpp"
+
+#include <sstream>
+
+std::string HttpResponse::to_string() const
+{
+    std::ostringstream out;
+    std::string reason;
+
+    switch (status) {
+    case 200: reason = "OK"; break;
+    case 404: reason = "Not Found"; break;
+    default: reason = "Unknown"; break;
+    }
+
+    out << "HTTP/1.1 " << status << "\r\n";
+    out << "Content-Type: " << content_type << "\r\n";
+    out << "Content-Length: " << body.size() << "\r\n";
+    out << "\r\n";
+    out << body;
+    return out.str();
+}
