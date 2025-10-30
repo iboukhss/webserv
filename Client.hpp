d5089c60e73062c77e974fa046dc56a61c3cfe32
Ismail <151830328+iboukhss@users.noreply.github.com>
2025-10-30 10:34:46 +0100

    feat: change the connection map to an intrusive list

--- Client.hpp
+++ Client.hpp
@@ -10,6 +10,13 @@ public:
     explicit Client(Socket* socket);
     ~Client();
 
+    Socket* socket() { return socket_; }
+    Client* next() { return next_; }
+    Client* prev() { return prev_; }
+
+    void set_next(Client* conn) { next_ = conn; }
+    void set_prev(Client* conn) { prev_ = conn; }
+
 private:
     Client();
     Client(const Client& other);
@@ -17,6 +24,8 @@ private:
 
 private:
     Socket* socket_;
+    Client* next_;
+    Client* prev_;
 };
 
 #endif // CLIENT_HPP_
