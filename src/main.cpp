b69bc48caa6cc06325ad52197a1ead9709308603
Dennis Heck <heckdennis257@gmail.com>
2026-01-12 12:23:58 +0100

    commit to run clang-format on laptop

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main(void)
     setup_signal_handlers();
 
     try {
-        HttpConfig cfg = load_http_config("config/vitepress.conf");
+        HttpConfig cfg = load_http_config("config/youpi_banane.conf");
 
         Server server(cfg.servers[0]);
         server.init();
